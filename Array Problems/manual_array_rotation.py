5c15f457f60bc13d23caa9316cf1a0bb71d47787
Krishankant89 <toxickush90@gmail.com>
2026-05-04 19:45:46 +0530

    manual array rotation

---
+++ Array Problems/manual_array_rotation.py
@@ -0,0 +1,44 @@
+def left_rotation(arr,k):
+    n = len(arr)
+    k = k % n
+    
+    result = []
+    
+    for i in range(k, n):
+        result.append(arr[i])
+    
+    for i in range(0, k):
+        result.append(arr[i])
+        
+    return result
+
+n = int(input("enter the size of array:"))
+
+arr = []
+for i in range(n):
+    x = int(input("Enter the elements:"))
+    arr.append(x)
+    
+k = int(input("Enter number of rotations:"))
+
+rotated = left_rotation(arr, k)
+
+print("Rotated array:", rotated)
+
+
+
+'''
+🔹 Split the Array
+Original:   [1, 2, 3, 4, 5]
+Index:       0  1  2  3  4
+k = 2
+
+Split at index k:
+
+First half  = [1, 2]        → index 0 to k-1
+Second half = [3, 4, 5]     → index k to n-1
+🔹 Combine (Swap Order)
+Rotated = Second half + First half
+        = [3, 4, 5] + [1, 2]
+        = [3, 4, 5, 1, 2]
+'''
